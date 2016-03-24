e04a3cf7b6ae954291cd9cd8de21483afcd9ad22
mgibbs189 <mgibbs189@gmail.com>
2016-03-24 11:51:38 -0400

    Mostly working functionality, minus nags

--- use-child-theme.php
+++ use-child-theme.php
@@ -28,29 +28,117 @@ if ( ! class_exists( 'Use_Child_Theme' ) ) {
 
     class Use_Child_Theme
     {
+
+        public $theme;
+        public $child_theme;
+
+
         function __construct() {
             add_action( 'admin_init', array( $this, 'admin_init' ) );
         }
 
 
         function admin_init() {
-            $theme = wp_get_theme();
-            echo '<pre>';var_dump( $theme );echo '</pre>';die();
+            $this->theme = wp_get_theme();
+
+            // Exit if this is a child theme
+            if ( false !== $this->theme->parent() ) {
+                return;
+            }
+            // Does the child theme exist?
+            elseif ( $this->has_child_theme() ) {
+                var_dump( 'USE THE CHILD THEME!' );
+            }
+            // Create a child theme
+            else {
+                $this->install_child_theme();
+            }
         }
 
 
+        /**
+         * Does this theme have a child?
+         */
+        function has_child_theme() {
+            $themes = wp_get_themes();
+            $folder_name = basename( $this->theme->get_stylesheet_directory() );
+
+            foreach ( $themes as $theme ) {
+                if ( $folder_name == $theme->get( 'Template' ) ) {
+                    $this->child_theme = $theme;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        /**
+         * Activate child theme
+         */
         function activate_child_theme() {
-
+            $slug = basename( $this->child_theme->get_stylesheet_directory() );
+            switch_theme( $slug );
         }
 
 
-        function is_theme_writable() {
-            
-        }
-
-
+        /**
+         * Create child theme
+         */
         function install_child_theme() {
+            $dir = $this->theme->get_stylesheet_directory() . '-child';
 
+            $replacements = array(
+                'theme_name' => $this->theme->get( 'Name' ) . ' Child',
+                'theme_uri' => $this->theme->get( 'ThemeURI' ),
+                'template' => basename( $this->theme->get_stylesheet_directory() ),
+                'version' => '1.0',
+            );
+
+            $css = $this->style_css();
+            foreach ( $replacements as $key => $str ) {
+                $css = str_replace( '{' . $key . '}', $str, $css );
+            }
+
+            if ( wp_mkdir_p( $dir ) ) {
+                file_put_contents( $dir . '/style.css', $css );
+                file_put_contents( $dir . '/functions.php', $this->functions_php() );
+            }
+        }
+
+
+        /**
+         * Generate the style.css contents
+         */
+        function style_css() {
+            ob_start();
+?>
+/*
+Theme Name:     {theme_name}
+Theme URI:      {theme_uri}
+Template:       {template}
+Version:        {version}
+*/
+<?php
+            return ob_get_clean();
+        }
+
+
+        /**
+         * Generate the functions.php contents
+         */
+        function functions_php() {
+            ob_start();
+?>
+<?php echo '<'; ?>?php
+
+function child_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles' );
+<?php
+            return ob_get_clean();
         }
     }
 

134bacf5c26af085c003a49d98ff3368a19704a4
John James Jacoby <johnjamesjacoby@me.com>
2016-03-29 18:18:22 -0500

    More thorough plugin header.

--- plugin.php
+++ plugin.php
@@ -1,25 +1,15 @@
 <?php
-/*
-Plugin Name: Use Child Theme
-Plugin URI: https://facetwp.com/
-Description: Encourage use of child themes
-Version: 1.0
-Author: Matt Gibbs
+/**
+ * Plugin Name: Use Child Theme
+ * Plugin URI:  https://facetwp.com/
+ * Author:      Matt Gibbs
+ * License:     GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Version:     1.0
+ * Description: Encourage use of child themes
+ * Text Domain: use-child-theme
+ */
 
-Copyright 2016 FacetWP, LLC
+// Copyright 2016 FacetWP, LLC
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, see <http://www.gnu.org/licenses/>.
-*/
-
-include( dirname( __FILE__ ) . '/use-child-theme.php' );
+include dirname( __FILE__ ) . '/use-child-theme.php';
